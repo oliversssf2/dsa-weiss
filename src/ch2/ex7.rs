2a9a442b64dc5f8bab3a19ba63c97d480e341eb8
fongsu <oliversssf2@gmail.com>
2022-07-06 12:35:57 +0800

    regular commit

---
+++ src/ch2/ex7.rs
@@ -0,0 +1,8 @@
+use rand::Rng;
+
+fn main() {
+    rand_num = Rng:
+    for i in vec![1,10,100,1000,10000] {
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// struct MyVector<T, const N: usize> {
+    // data: Box<[T; N]>,
+    // the_size: usize,
+    // the_capacity: usize,
+// }
+
+// impl<T, const N: usize> MyVector<T, N> {
+    // fn new(size: usize) -> MyVector<T, N>{
+        // MyVector { data: (), the_size: (), the_capacity: () }
+    // }
+// }
+
+
+
+
+fn main() {
+}
